6f433614ca100135407f36160c45e77556a55ac0
Muhammad Huzaifa <cs15lwi22atp@ieng6-201.ucsd.edu>
2022-02-04 14:45:07 -0800

    Fixed test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void additionTest() {
-        assertNotEquals(2, SkillDemo.SkillDemoAdd(1, 1));
+        assertEquals(2, SkillDemo.SkillDemoAdd(1, 1));
     }
 }
